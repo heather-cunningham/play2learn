d9ff3975f56183dd55217155f04094369808a611
heather-irene <cunningham.heatherirene@gmail.com>
2024-10-26 12:26:36 -0400

    homepage: Set up app.js to use Node and Express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8081;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (request, response)=>{
+    response.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port);
