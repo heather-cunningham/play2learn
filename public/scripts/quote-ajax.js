7471ba29d7957cc2e13d3b3a5ccfe97b751a601d
heather-irene <cunningham.heatherirene@gmail.com>
2024-10-29 12:26:49 -0400

    homepage: Quotes: Comment to instructor about couldn't get XMLHttpRequest syntax to work

--- public/scripts/quote-ajax.js
+++ public/scripts/quote-ajax.js
@@ -1,5 +1,12 @@
 const blockQuote = document.getElementsByClassName("testimonial-quote")[0];
 
+// Note to Jared or the grading instructor:  I could not get the XMLHttpRequest type 
+// syntax to work with loading the quotes.  The readystate never changed to DONE or 4,
+// and I could not figure out what I was doing wrong.  I tried following the "presidents" Demo,
+// but I could not replicate that syntax and get it to work.
+// So, I did it w/ async fetch instead.
+// The Node and AJAX lessons still don't make sense to me fully.  I still don't really understand 
+// those lessons.  Backend and server side code is a weak point for me.   
 const fetchQuotes = async () => {
   try{
     const response = await fetch("/testimonials");
